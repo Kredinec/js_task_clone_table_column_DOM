bee60dc7f67e0f3e6017a80b16f4f6c341514469
Vitalii Kredinets <kredinec.v@gmail.com>
2025-02-06 16:57:54 +0200

    make build

---
+++ index.3e1bac90.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("tr").forEach(function(e){var n=e.children;if(n.length>=5){var t=n[1].cloneNode(!0);e.insertBefore(t,n[4])}})});
+//# sourceMappingURL=index.3e1bac90.js.map
