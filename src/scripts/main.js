93fea0da9f7558cd78d64da44b1c8290e9ffd947
Vitalii Kredinets <kredinec.v@gmail.com>
2025-02-06 16:42:02 +0200

    solve task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const rows = document.querySelectorAll('tr');
+
+  rows.forEach((row) => {
+    const cells = row.children;
+    const secondCol = cells[1];
+    const copy = secondCol.cloneNode(true);
+
+    row.insertBefore(copy, cells[4]);
+  });
+});
